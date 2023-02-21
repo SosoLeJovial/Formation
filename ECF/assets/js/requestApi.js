2f5b5ddaa8b59095ffcb22ba382a4a5375486d79
Tebourbi Sofien <104938050+SosoLeJovial@users.noreply.github.com>
2023-02-22 00:16:18 +0100

    add gallery image suggestion, set new gallery style for research

--- ECF/assets/js/requestApi.js
+++ ECF/assets/js/requestApi.js
@@ -9,14 +9,73 @@ let buttonSearch = document.querySelector('#search')
 let yearMovie = document.querySelector('#yearMovie')
 let currentPage = 1;
 
+function accueil() {
+    let displaySearch = document.querySelector('#resultRequest');
+    displaySearch.innerHTML = "";
+    const titleSelection = document.createElement('h3');
+    titleSelection.id = "titleSelection";
+    titleSelection.innerHTML = "Notre Selection";
+    displaySearch.appendChild(titleSelection);
+    const galleryImage = document.createElement('div');
+    galleryImage.id = "galleryImage";
+    displaySearch.appendChild(galleryImage);
+
+    fetch(`http://www.omdbapi.com/?apikey=431b17b0&r=json&s=avengers`)
+        .then(response => response.json())
+        .then(data => {
+
+            if (data.Response === 'False') {
+                swal({
+                    icon: 'error',
+                    text: "Aucun résultat n'a été trouvé",
+                    timer: '2000',
+                })
+            }
+            else {
+                const results = data.Search.slice(0, 9);
+                results.forEach(movie => {
+                    const item = document.createElement('figure');
+                    let br = document.createElement('br');
+                    let poster = movie.Poster;
+                    item.classList.add('imgGallery');
+
+                    let legendHover = document.createElement('figcaption');
+                    legendHover.classList.add('legendHover')
+                    legendHover.innerHTML = `${movie.Title}${movie.Year}`;
+                    legendHover.insertBefore(br,legendHover.lastChild);
+                    legendHover.style.display = "none";
+                    item.appendChild(legendHover);
+                    if (poster !== 'N/A') {
+
+                        let img = document.createElement('img');
+                        img.src = poster;
+                        item.appendChild(img);
+                    }
+                    displaySearch.appendChild(item);
+                    item.addEventListener('mouseover', function () {
+                        legendHover.style.display = "block";
+                    })
+                    item.addEventListener('mouseout', function () {
+                        legendHover.style.display = "none";
+                    })
+                })
+            }
+        });
+    ;
+}
+
+accueil();
+// document.addEventListener("load", accueil);
+
+
 
 function searchOmdb(page) {
     pagination.innerHTML = "";
-    if(page == undefined){
+    if (page == undefined) {
         page = 1;
     }
 
-    let displaySearch = document.querySelector('#resultRequest')
+    let displaySearch = document.querySelector('#resultRequest');
     displaySearch.innerHTML = "";
     let query = `http://www.omdbapi.com/?apikey=431b17b0&page=${page}&r=json`;
 
@@ -55,55 +114,65 @@ function searchOmdb(page) {
 
             function affichage(data) {
                 data.Search.forEach(movie => {
-                const item = document.createElement('div');
-                item.id = "cardResult";
-                let poster = movie.Poster;
-                item.innerHTML = `
-                <h2>${movie.Title}</h2>
-                <p>${movie.Year}</p>
-                `;
-                if (poster !== 'N/A') {
-                    let img = document.createElement('img');
-                    img.src = poster;
-                    item.appendChild(img);
-                }
-                if (poster == 'N/A') {
-                    let img = document.createElement('img');
-                    img.src = './assets/img/errorNoPngOups.png' ;
-                    item.appendChild(img);
-                }
-                displaySearch.appendChild(item);
-            })
-            } 
-            let numberOfPage = Math.floor(data.totalResults/data.Search.length);
-            if(numberOfPage >= 10){
+                        const results = data.Search.slice(0, 9);
+                        results.forEach(movie => {
+                            const item = document.createElement('figure');
+                            let br = document.createElement('br');
+                            let poster = movie.Poster;
+                            item.classList.add('imgGallery');
+        
+                            let legendHover = document.createElement('figcaption');
+                            legendHover.classList.add('legendHover')
+                            legendHover.innerHTML = `${movie.Title}${movie.Year}`;
+                            legendHover.insertBefore(br,legendHover.lastChild);
+                            legendHover.style.display = "none";
+                            item.appendChild(legendHover);
+                            if (poster !== 'N/A') {
+        
+                                let img = document.createElement('img');
+                                img.src = poster;
+                                item.appendChild(img);
+                            }
+                            displaySearch.appendChild(item);
+                            item.addEventListener('mouseover', function () {
+                                legendHover.style.display = "block";
+                            })
+                            item.addEventListener('mouseout', function () {
+                                legendHover.style.display = "none";
+                            })
+                        })
+                    }
+                )
+            }
+            let numberOfPage = Math.floor(data.totalResults / data.Search.length);
+            if (numberOfPage >= 10) {
                 numberOfPage = 10;
             }
-            
+
             affichage(data);
 
             pagination(numberOfPage)
-            function pagination(pages){
+            function pagination(pages) {
                 let pagination = document.querySelector('#pagination');
                 let ul = document.createElement('ul');
                 ul.classList.add('pagination');
-                for(let i =1; i <= pages; i++){
+                for (let i = 1; i <= pages; i++) {
                     let li = document.createElement('li');
                     li.classList.add('page-item');
                     let a = document.createElement('a');
                     a.classList.add('page-link');
-                    a.href = i; 
+                    a.href = i;
                     a.innerHTML = i;
-                    a.addEventListener('click', function(e) {
+                    a.addEventListener('click', function (e) {
                         e.preventDefault();
-                       searchOmdb(this.getAttribute('href'))
+                        searchOmdb(this.getAttribute('href'))
                     })
                     li.appendChild(a);
                     ul.appendChild(li);
                 }
                 pagination.appendChild(ul)
             }
-            
+
         })
         .catch(error => console.error(error));
 };
